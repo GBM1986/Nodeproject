2d7612dc626012c4ad25511b7c3295878b5b2bcf
GBM1986 <gunnar_bjarni@hotmail.com>
2023-11-23 10:25:52 +0100

    a

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import http from 'http';
 
 http.createServer((request, response) => {
-    console.log('Hello world');
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write('Hello World!');
+    response.end();
 }).listen(4000)

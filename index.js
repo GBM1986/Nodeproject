e9b5e7acae5a0dd410cc5649ddc8d2182b2f7f49
GBM1986 <gunnar_bjarni@hotmail.com>
2023-11-23 12:33:36 +0100

    blahblah

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import http from 'http';
 
 http.createServer((request, response) => {
     response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write('Hello World! !!!!!!!');
+    response.write('Hello World! hellohelllohello!!!!!!!');
     response.end();
 }).listen(4000)
 
